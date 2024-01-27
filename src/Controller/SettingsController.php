eeafef9aaeccc6b7d982217e1d98e562ec78a12e
Natalia Galicka <natalia.galicka.programista@gmail.com>
2024-01-27 06:30:46 +0100

    Create method to edit users settings

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\UserProfile;
 use App\Form\WeddingSettingsType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class SettingsController extends AbstractController
 {
@@ -42,5 +44,109 @@ class SettingsController extends AbstractController
             'email' => $email,
             'form' => $form->createView()
         ]);
+    }
+
+    #[Route('/settings/email/{id}', name: 'update_email', methods: 'POST')]
+    public function updateEmail(Request $request, EntityManagerInterface $entityManager,  $id)
+    {
+        $email = trim($request->request->get('email'));
+
+        if (empty($email))
+            return $this->redirectToRoute('app_settings');
+
+        $user = $entityManager->getRepository(User::class)->find($id);
+
+        $user->setEmail($email);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_settings');
+    }
+
+    #[Route('/settings/password/{id}', name: 'update_password', methods: 'POST')]
+    public function updatePassword(Request $request, EntityManagerInterface $entityManager,  $id)
+    {
+        $newPassword = trim($request->request->get('password'));
+
+        if (empty($newPassword))
+            return $this->redirectToRoute('app_settings');
+
+        $user = $entityManager->getRepository(User::class)->find($id);
+
+        // Check if user exists
+        if (!$user) {
+            // Handle the case where the user with the given ID is not found
+            return $this->redirectToRoute('app_settings');
+        }
+
+        $user->setPassword($newPassword);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_settings');
+    }
+
+
+    #[Route('/settings/brideName/{id}', name: 'update_brideName', methods: 'POST')]
+    public function updateBrideName(Request $request, $id, EntityManagerInterface $entityManager)
+    {
+        $newBrideName = trim($request->request->get('brideName'));
+
+        if (empty($newBrideName))
+            return $this->redirectToRoute('app_settings');
+
+
+        $settings = $entityManager->getRepository(UserProfile::class)->find($id);
+
+
+        $settings->setBrideName($newBrideName);
+
+        $entityManager->persist($settings);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_settings');
+    }
+
+    #[Route('/settings/groomName/{id}', name: 'update_groomName', methods: 'POST')]
+    public function updateGroomName(Request $request, $id, EntityManagerInterface $entityManager)
+    {
+        $newGroomName = trim($request->request->get('groomName'));
+
+        if (empty($newGroomName))
+            return $this->redirectToRoute('app_settings');
+
+        $settings = $entityManager->getRepository(UserProfile::class)->find($id);
+
+        $settings->setGroomName($newGroomName);
+
+        $entityManager->persist($settings);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_settings');
+    }
+
+
+    #[Route('/settings/date/{id}', name: 'update_date', methods: 'POST')]
+    public function updateDate(Request $request, $id, EntityManagerInterface $entityManager)
+    {
+        $newDate = trim($request->request->get('date'));
+
+        $newDateTime = new \DateTime($newDate);
+
+        if (empty($newDate))
+            return $this->redirectToRoute('app_settings');
+
+        $settings = $entityManager->getRepository(UserProfile::class)->find($id);
+
+        var_dump($newDate);
+
+        $settings->setWeddingDate($newDateTime);
+
+        $entityManager->persist($settings);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_settings');
     }
 }
